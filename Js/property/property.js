cd1b0a9850fa62499edb402059e4445cd7dbc02a
XiaoMingLi <lxm@upup.cn>
2018-06-19 18:06:14 +0800

    comimit

--- Js/property/property.js
+++ Js/property/property.js
@@ -1,6 +1,6 @@
 ;(function() {
-    $("#property .tab").eq(1).addClass("on");
-    $("#property .property-item").eq(1).addClass("on");
+    $("#property .tab").eq(2).addClass("on");
+    $("#property .property-item").eq(2).addClass("on");
     $("#property .tab").click(function() {
         $("#property .tab").removeClass("on").eq($(this).index()).addClass("on");
         $("#property .property-item").removeClass("on").eq($(this).index()).addClass("on");

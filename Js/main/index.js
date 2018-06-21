6f7be0cbb194cf3f6fdcb48368dbaea83ac1c08e
XiaoMingLi <XiaoMingLi@github.com>
2018-06-22 00:37:02 +0800

    commit

--- Js/main/index.js
+++ Js/main/index.js
@@ -80,7 +80,7 @@ $(document).ready(function() {
     var owl = $("#owl-demo");
     owl.owlCarousel({
  
-        // autoPlay: 4000, 
+        autoPlay: 4000, 
         items: 3,
         itemsDesktop : [1199,3],
         itemsDesktopSmall : [979,3],

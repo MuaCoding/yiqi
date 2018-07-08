58308eb41a999701fb43b9da0c4505767b31f497
XiaoMingLi <XiaoMingLi@github.com>
2018-07-09 00:40:11 +0800

    commit

--- Js/business/business.js
+++ Js/business/business.js
@@ -1,6 +1,6 @@
 ;(function() {
-    $("#business .tab").eq(1).addClass("on");
-    $("#business .business-item").eq(1).addClass("on");
+    $("#business .tab").eq(0).addClass("on");
+    $("#business .business-item").eq(0).addClass("on");
     $("#business .tab").click(function() {
         $("#business .tab").removeClass("on").eq($(this).index()).addClass("on");
         $("#business .business-item").removeClass("on").eq($(this).index()).addClass("on");
